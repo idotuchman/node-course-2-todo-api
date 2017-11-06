da359f72f78e60c1ea91ac52ae94213e1147e15a
IT <ido_tuchman@yahoo.com>
2017-11-05 20:36:44 -0800

    Add GET /todos/:id

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,41 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '59ffad8a5288c7274401c84dxx';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('Id not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+var id ='59fe689f4131dd471437c15f';
+
+User.findById(id).then((user) => {
+  if (!user) {
+    return console.log('User id not found');
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb')
 
 const {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +27,34 @@ app.get('/todos', (req, res) => {
   }), (e) => {
     res.status(400).send(e);
   }
+});
+
+//GET /todos/132343
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+  //validate id using isValid
+    // 404 - send back empty send
+
+  //findById
+    // success
+      // if todo - send it back
+      // if no todo - 404 with empty body
+    // error
+      // 400 - and send empty body back
+
 });
 
 app.listen(3000, () => {
